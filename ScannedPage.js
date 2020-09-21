9cfd46a6f11971f6457c68856c1d8014aa251599
Unknown <claudiu@megheaiulian.com>
2020-09-21 19:53:56 +0300

    Show data from firebase to console

--- ScannedPage.js
+++ ScannedPage.js
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Alert, Modal, TouchableHighlight } from 'react-native';
 import { NativeRouter, Route, Link } from "react-router-native";
 
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+async function readScann() {
+  console.warn('readScann is called !!!');
+  const scanns = firebase.firestore().collection('scan');
+  scanns.get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      console.warn(doc.id, " => ", doc.data());
+    });
+  })
+}
+
 export default function App() {
 
   return (
@@ -17,6 +31,9 @@ export default function App() {
       }}>
         Here is page with results after scanned - returned by firebase.
       </Text>
+      <Button
+        title={'Show data'}
+        onPress={() => readScann()}/>
     </View>
   );
 }
