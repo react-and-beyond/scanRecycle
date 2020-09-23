f10329bbbfdb5c6f989501f93b6b75ad8508c37f
Unknown <claudiu@megheaiulian.com>
2020-09-23 13:49:00 +0300

    Get data from firebase to a scanned page with flatlist

--- ScannedPage.js
+++ ScannedPage.js
@@ -1,19 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Alert, Modal, TouchableHighlight } from 'react-native';
+import { FlatList, ActivityIndicator, Text, Button, View,  TouchableHighlight } from 'react-native';
 import { NativeRouter, Route, Link } from "react-router-native";
 
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-async function readScann() {
-  console.warn('readScann is called !!!');
-  const scanns = firebase.firestore().collection('scan');
-  scanns.get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      console.warn(doc.id, " => ", doc.data());
-    });
-  })
+// async function readScann() {
+//   console.warn('readScann is called !!!');
+//   const scanns = firebase.firestore().collection('scan');
+//   scanns.get().then(function(querySnapshot) {
+//     querySnapshot.forEach(function(doc) {
+//       console.warn(doc.id, " => ", doc.data());
+//     });
+//   })
+// }
+
+function Scans() {
+  const [loading, setLoading] = useState(true);
+  const [scans, setScans] = useState([]);
+
+  useEffect(() => {
+    const scaned = firebase.firestore()
+      .collection('scan')
+      .get()
+      .then(querySnapshot => {
+        const scans = [];
+        // console.warn('querySnapshot: ',querySnapshot);
+        querySnapshot.forEach(documentSnapshot => {
+          scans.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+        // console.warn('scans', scans)
+        setScans(scans);
+        setLoading(false);
+      });
+
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  console.warn('scans',scans)
+  return (
+    <FlatList
+      data={scans}
+      renderItem={({ item }) => (
+        <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>ID: {item.id}</Text>
+          <Text>Name {item.name}</Text>
+          <Text>Barcode: {item.barcode}</Text>
+        </View>
+      )}
+    />
+  );
 }
 
 export default function App() {
@@ -31,9 +73,7 @@ export default function App() {
       }}>
         Here is page with results after scanned - returned by firebase.
       </Text>
-      <Button
-        title={'Show data'}
-        onPress={() => readScann()}/>
+      <Scans/>
     </View>
   );
 }
