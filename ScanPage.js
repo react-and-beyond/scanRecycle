a0c31033e51258b46e5defc221454df19bb87414
Unknown <claudiu@megheaiulian.com>
2020-11-09 14:43:22 +0200

    Scan page update
    Clear some alerts & console warn, move style on top, create condition if product scanned exist in firestore

--- ScanPage.js
+++ ScanPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { TextInput, Text, View, StyleSheet, Button, Alert, Modal, TouchableHighlight } from 'react-native';
+import { TextInput, Text, View, StyleSheet, Button, Modal, TouchableHighlight } from 'react-native';
 import { NativeRouter, Route, Link } from "react-router-native";
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import styled from 'styled-components/native'
@@ -29,10 +29,10 @@ const CustomTextInput = styled.TextInput`
   padding: 5px;
   margin: 5px;
 `
+
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(null);
-  const [nameBarcode, setNameBarcode] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -43,11 +43,9 @@ export default function App() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(data);
-    //console.warn(data.toString());
     setTimeout(function(){
       setScanned(null);
-    }, 910000);
-    //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    }, 5000000);
   };
 
 
@@ -58,25 +56,103 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
-  async function addScann() {
-    //console.warn('addScann is called !!!');
-    const today = new Date();
-    const date = today.getFullYear() + "/"+ parseInt(today.getMonth()+1) +"/"+ today.getDate();
-    const scanns = firebase.firestore().collection('scan');
-    const scan = scanns.doc(nameBarcode.toString());
-    const data = await scan.set({
-      name: nameBarcode,
-      barcode: scanned,
-      createdAt: date
-    })
-    .then(function(){
-      console.warn('Document successfully added!');
-      setScanned(null);
-    })
-    .catch(function(error){
-      console.warn('Error added document: ', error);
-    })
-}
+  function BarcodeChecker({barcode}) {
+    const firebaseCollection = firebase.firestore().collection('scan').where('barcode', '==', barcode);
+    const [doc, setDoc] = useState(null);
+    const [nameBarcode, setNameBarcode] = useState('');
+
+    async function addScann() {
+      const today = new Date();
+      const date = today.getFullYear() + "/"+ parseInt(today.getMonth()+1) +"/"+ today.getDate();
+      const scanns = firebase.firestore().collection('scan');
+      const scan = scanns.doc(nameBarcode.toString());
+      const data = await scan.set({
+        name: nameBarcode,
+        barcode: scanned,
+        createdAt: date
+      })
+      .then(function(){
+        console.warn('Document successfully added!');
+        setScanned(null);
+      })
+      .catch(function(error){
+        console.warn('Error added document: ', error);
+      })
+    }
+
+    useEffect(() => {
+      (async() => {
+        try{
+          const doc = await firebaseCollection.get();
+          setDoc(doc);
+        }catch(error){
+          console.warn("Error getting document:", error);
+        }
+      })();
+    }, []);
+    return doc?.size ? (
+
+      <StyledView>
+        <Text>Exist in firebase</Text>
+        <Text>Title doc</Text>
+        <CustomTextInput
+          value={barcode}
+          editable={false}
+        />
+        <Text>Name doc</Text>
+        <CustomTextInput
+          value="value"
+          placeholder="placeholder"
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}>
+          <Button
+            color="#209d7e"
+            title={'Cancel'}
+            onPress={() => setScanned(null)}
+          />
+          <Button
+            color="#ede331"
+            title={'Update product'}
+            onPress={() => updateScann()}
+          />
+        </View>
+      </StyledView>
+    ):(
+      <StyledView>
+        <Text>Not exist in firebase</Text>
+        <CustomTextInput
+        value={scanned}
+        editable={false}
+        />
+        <CustomTextInput
+          placeholder="Insert name"
+          onChangeText={setNameBarcode}
+          value={nameBarcode}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}>
+          <Button
+            color="#209d7e"
+            title={'Cancel'}
+            onPress={() => setScanned(null)}
+          />
+          <Button
+            color="#6cbc1b"
+            title={'Save'}
+            onPress={() => addScann()}/>
+        </View>
+      </StyledView>
+    )
+  }
 
   return (
     <ScanPageWrapper>
@@ -88,34 +164,7 @@ export default function App() {
         visible={!!scanned}
         transparent={true}
         >
-        <StyledView>
-          <Text>Result after scan:</Text>
-          <CustomTextInput
-            value={scanned}
-            editable={false}
-          />
-          <CustomTextInput
-            placeholder="Insert name"
-            onChangeText={setNameBarcode}
-            value={nameBarcode}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              width: '100%',
-            }}>
-            <Button
-              color="#ede331"
-              title={'Cancel'}
-              onPress={() => setScanned(null)}
-            />
-            <Button
-              color="#6cbc1b"
-              title={'Save'}
-              onPress={addScann}/>
-          </View>
-        </StyledView>
+          <BarcodeChecker barcode={scanned} />
       </Modal>
     </ScanPageWrapper>
   );
